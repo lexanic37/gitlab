f824869b44ce6a5e7afba64698162fd540ee29c7
Алексей Хайдин <lexanic37@gmail.com>
2017-07-30 00:48:00 +0300

    Initial commit

--- js/js.js
+++ js/js.js
@@ -1,2 +1,47 @@
 $('#fullpage').fullpage({anchors:['onePage', 'twoPage', 'threePage', 'forPage', 'fifthPage'],
 menu: '#mainMenu'});
+(function() {
+  var removeSuccess;
+
+  removeSuccess = function() {
+    return $('.button').removeClass('success');
+  };
+
+  $(document).ready(function() {
+    return $('.button').click(function() {
+      $(this).addClass('success');
+      return setTimeout(removeSuccess, 3000);
+    });
+  });
+
+}).call(this);
+(function() {
+  var removeSuccess;
+
+  removeSuccess = function() {
+    return $('.buttonmap').removeClass('success');
+  };
+
+  $(document).ready(function() {
+    return $('.buttonmap').click(function() {
+      $(this).addClass('success');
+      return setTimeout(removeSuccess, 3000);
+    });
+  });
+
+}).call(this);
+(function() {
+  var removeSuccess;
+
+  removeSuccess = function() {
+    return $('.buttoncost').removeClass('success');
+  };
+
+  $(document).ready(function() {
+    return $('.buttoncost').click(function() {
+      $(this).addClass('success');
+      return setTimeout(removeSuccess, 3000);
+    });
+  });
+
+}).call(this);
